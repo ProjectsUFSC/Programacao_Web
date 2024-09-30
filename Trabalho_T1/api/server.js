1b7845850c087aa9fd0602e138eeb9ed977ab5ec
Daleffe <augustodaleffe@gmail.com>
2024-09-30 15:19:34 -0300

    alterando o servidor para incorporar as rotas criadas

---
+++ Trabalho_T1/api/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import router from './routes/index.js';
+
+const app = express();
+const PORT = process.env.PORT || 2000;
+
+app.get('/', (req, res) => {
+    res.send('Servidor online');
+});
+
+app.use(router)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
