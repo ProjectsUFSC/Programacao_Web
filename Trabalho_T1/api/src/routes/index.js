b25ce4f809d4f4821e56489f16db9b6e2d57130a
Daleffe <augustodaleffe@gmail.com>
2024-09-30 15:18:54 -0300

    adiconando as rotas base que serão utilizadas

---
+++ Trabalho_T1/api/src/routes/index.js
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/', (req, res) => {
+    res.send('Servidor online');
+});
+
+router.get('/lista', (req, res) => {
+    res.send('Lista');
+});
+
+router.post('/login', (req, res) => {
+    res.send('Login');
+});
+
+router.post('/abre', (req, res) => {
+    res.send('Abre');
+});
+
+export default router;
