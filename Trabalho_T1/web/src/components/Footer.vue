06430451c2b841529a5aaba64cdddacfc40e1500
Daleffe <augustodaleffe@gmail.com>
2024-10-01 15:14:12 -0300

    criando as componentes Footer e Header para o projeto

---
+++ Trabalho_T1/web/src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="footer">
+
+        <p>&copy;    2024 - Desenvolvido por <a href="https://github.com/Dleffe" target="_black">Augusto Daleffe</a></p>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Footer',
+}
+</script>
+
+<style scoped>
+
+    #footer {
+        display: flex;
+        justify-content: start;
+        align-items: end;
+        background-color: #002b76;
+        color: white;
+        padding: 10px;
+        height: 80px;
+        width: 100%;
+        position: absolute;
+        bottom: 0px;
+    }
+
+    #footer a{
+        color: rgb(255, 255, 255);
+        text-decoration: none;
+    }
+</style>
---
+++ Trabalho_T1/web/src/components/Header.vue
@@ -0,0 +1,57 @@
+<template>
+    <div id="navbar">
+        <div class="navbar-item">
+            <a href=""><img src="/imgs/ufsc-logo.png" alt="Logo UFSC"></a>
+            <h1>Sistema de Acesso de Salas UFSC - Araranguá</h1>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'Header',
+    data() {
+        return {
+            login: false
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+    boddy {
+        margin: 0;
+        padding: 0;
+    }
+
+    #navbar {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: #002b76;
+        color: white;
+        padding: 10px;
+        height: 80px;
+    }
+
+    .navbar-item {
+        display: flex;
+        align-items: center;
+    }
+
+    .navbar-item a{
+        color: rgb(255, 255, 255);
+        text-decoration: none;
+        margin: 10px;
+    }
+
+    .navbar-item img {
+        width: auto;
+        height: 80px;
+    }
+
+
+
+</style>
