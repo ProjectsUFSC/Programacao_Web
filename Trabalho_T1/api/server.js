564ddb88f88253fc321c0dfed5d0c652259028eb
Daleffe <augustodaleffe@gmail.com>
2024-10-03 14:42:49 -0300

    adicionando a conecxão com o banco de dados mongoDB

--- Trabalho_T1/api/server.js
+++ Trabalho_T1/api/server.js
@@ -1,17 +1,24 @@
-import express from "express";
-import router from './src/routes/index.js';
+import express from "express"
+import mongoose from "mongoose"
+import router from './src/routes/index.js'
 
-const app = express();
-const PORT = process.env.PORT || 2000;
+const app = express()
+const PORT = process.env.PORT || 2000
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/controlePortas'
 
-app.get('/', (req, res) => {
-    res.send('Servidor online');
+mongoose.connect(MONGO_URL)
+
+const db = mongoose.connection;
+db.on('error', (error) => console.error('Erro ao conectar ao MongoDB:', error));
+db.once('open', () => {
+    console.log('Conexão com MongoDB estabelecida com sucesso');
 });
 
+app.use(express.json())
 app.use(router)
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`)
 });
 
 
