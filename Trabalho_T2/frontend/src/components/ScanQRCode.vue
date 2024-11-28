5ea30d18ee7379b4ab3ad44e7429c94e87d6277c
Daleffe <105308194+Dleffe@users.noreply.github.com>
2024-11-28 15:12:35 -0300

    Revert "tirei https do back arrumei o pushsubscription, falta o scan funciona…"

--- Trabalho_T2/frontend/src/components/ScanQRCode.vue
+++ Trabalho_T2/frontend/src/components/ScanQRCode.vue
@@ -29,35 +29,33 @@ export default {
       false
     );
 
-    qrScanner.render(this.registerQrCode);
+    qrScanner.render(this.registerQrCode, this.onScanError);
 
-    this.getUserQrCodes();
+    this.fetchUserQrCodes();
   },
   methods: {
-    async getUserQrCodes() {
+    async fetchUserQrCodes() {
       const token = localStorage.getItem("token");
       const username = localStorage.getItem("username");
       try {
-      const response = await axios.get("http://localhost:3000/api/user-qr-codes", {
-      params: { username: username },
-      headers: { Authorization: `Bearer ${token}` },
-      });
-      this.userQrCodes = response.data.codes;
+        const response = await axios.get("https://localhost:3000/api/user-qr-codes",
+        { username: username }, 
+        { headers: { Authorization: `Bearer ${token}` },});
+        this.userQrCodes = response.data.codes;
       } catch (error) {
         console.error("Erro ao buscar QR-Codes cadastrados:", error);
       }
     },
     async registerQrCode(decodedText) {
       const token = localStorage.getItem("token");
-      const username = localStorage.getItem("username");
       try {
         await axios.post(
           "http://localhost:3000/api/qr-codes",
-          { code: decodedText, username: username},
+          { code: decodedText },
           { headers: { Authorization: `Bearer ${token}` } }
         );
         alert("QR-Code registrado com sucesso!");
-        this.getUserQrCodes(); 
+        this.fetchUserQrCodes(); 
       } catch (error) {
         console.error("Erro ao registrar QR-Code:", error);
         alert("Erro ao registrar QR-Code. Tente novamente.");
